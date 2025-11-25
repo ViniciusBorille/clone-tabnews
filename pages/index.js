9da12c935e2173ed645291e69cc6516e1dc8fa38
Vinicius <viniciusborilledasilva@gmail.com>
2025-11-25 20:22:37 -0300

    Criação da página em `desenvolvimento` e arquivo `index.css`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
+import React from "react";
+import "./index.css";
+
 function Home() {
-  return <h1>Me deve uma massagem!!!</h1>;
+  return;
+  <div className="container-em-desenvolvimento">
+    <div className="conteudo-em-desenvolvimento">
+      <h1>Em Desenvolvimento</h1>
+      <p>Estamos trabalhando para trazer novidades em breve!</p>
+      <div className="spinner"></div> {/* Animação de carregamento */}
+    </div>
+  </div>;
 }
 
 export default Home;
